2df2a234f9d273eec5797a7a7816c0ce726310e6
waxx567 <waynem567@gmail.com>
2023-09-15 17:56:16 +0200

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      GoGasGo delivery service
+      GoGasGo delivery service { import.meta.env.VITE_API_URL}
     </div>
   )
 }
