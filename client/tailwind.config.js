97af550b083d5333d807d32bb9692e89a27de0ec
waxx567 <waynem567@gmail.com>
2023-09-16 11:49:51 +0200

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+    ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blueColor': '#2a68ff',
+        'greyIsh': '#f1f4f8',
+        'cardShadow': '#f7f8f9',
+        'textColor': '#252b36',
+      }
+    },
   },
   plugins: [],
 }
