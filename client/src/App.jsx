2fb3eaa8ba3b589031f0222fa66f043d22ec4013
waxx567 <waynem567@gmail.com>
2023-09-17 09:54:51 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import NavBar from './components/NavBar/NavBar'
+import Search from './components/Search/Search'
+import Jobs from './components/Jobs/Jobs'
+import Value from './components/Value/Value'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Search />
+      <Jobs />
+      <Value />
+      <Footer />
     </div>
   )
 }
