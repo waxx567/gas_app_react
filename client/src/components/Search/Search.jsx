9000fc3e64d3402ce9e8fa70e38b6b4e4f8a391a
waxx567 <waynem567@gmail.com>
2023-09-17 11:40:50 +0200

    Update Search.jsx

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
+import { AiOutlineSearch } from 'react-icons/ai'
+
 const Search = () => {
   return (
     <div className='searchDiv grid gp-10 bg-greyIsh rounded-[10px] p-[3rem]'>
       <form action=''>
         <div className='firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700'>
-
+            <div className='flex gap-2 items-center'>
+                <AiOutlineSearch/>
+            </div>
         </div>
       </form>
     </div>
