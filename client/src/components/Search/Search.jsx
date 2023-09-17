19b54bcf49ad27c92bce34833d9f95d87baf7be0
waxx567 <waynem567@gmail.com>
2023-09-17 11:47:11 +0200

    Update Search.jsx

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -9,6 +9,7 @@ const Search = () => {
         <div className='firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700'>
             <div className='flex gap-2 items-center'>
                 <AiOutlineSearch className='text-[25px] icon'/>
+                <input type='text' className='bg-transparent text-blue-500 focus:outline-none w-[100%]' />
             </div>
         </div>
       </form>
