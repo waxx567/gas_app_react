1893b28553ff6e4411ea1561beec0bd5fff8ffba
waxx567 <waynem567@gmail.com>
2023-09-17 10:49:50 +0200

    Update Search.jsx

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div className='searchDiv grid gp-10 bg-greyIsh'>
-      Search
+    <div className='searchDiv grid gp-10 bg-greyIsh rounded-[10px] p-[3rem]'>
+      <form action=''>
+        <div className='firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700'>
+
+        </div>
+      </form>
     </div>
   )
 }
