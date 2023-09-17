7d922ee530ec17deac18d42241810d6ee07888c1
waxx567 <waynem567@gmail.com>
2023-09-17 10:05:16 +0200

    Update NavBar.jsx

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -4,7 +4,9 @@ const NavBar = () => {
   return (
     <div className='navBar flex justify-between items-center p-[3rem]'>
       <div className='logoDiv'>
-        <h1 className='logo text-[25px] text-blueColor'>Go<strong>Gas</strong>Go</h1>
+        <h1 className='logo text-[25px] text-blueColor'>
+          Go<strong>Gas</strong>Go
+        </h1>
       </div>
     </div>
   )
