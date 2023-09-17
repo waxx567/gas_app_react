cfff1d444bab3dc7bbb559db418194cccd085f29
waxx567 <waynem567@gmail.com>
2023-09-17 11:37:07 +0200

    Update Search.jsx

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -4,7 +4,7 @@ const Search = () => {
   return (
     <div className='searchDiv grid gp-10 bg-greyIsh rounded-[10px] p-[3rem]'>
       <form action=''>
-        <div className='firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh'>
+        <div className='firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700'>
 
         </div>
       </form>
