d657556e94ea45c185d881ad3f8ecee4b5c75d64
waxx567 <waynem567@gmail.com>
2023-09-18 20:56:02 +0200

    Update Value.jsx

--- client/src/components/Value/Value.jsx
+++ client/src/components/Value/Value.jsx
@@ -8,11 +8,11 @@ const Value = () => {
     <div className='mt-[2rem] mb-[4rem]'>
         
       <h1 className='text-textColor text-[22px] py-[2rem] pb-[.5rem] font-bold w-[400px] block'>Try us for service and convenience</h1>
+      
       <h2 className='text-textColor text-[20px] py-[2rem] pb-[3rem] font-semibold w-[400px] block'>We accept these cards:</h2>
 
-      <img src={logos} alt='card logos' className='m-auto w-[40%]' />
+      <img src={logos} alt='card logos' className='m-auto w-[250px] max-w-[50%]' />
       
-
     </div>
   )
 }
