660edf323c1fa4bca6cb13dfedfca76ce3612948
waxx567 <waynem567@gmail.com>
2023-09-18 14:15:08 +0200

    Update Search.jsx

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -36,7 +36,9 @@ const Search = () => {
       <div className='secDiv flex items-center gap-10 justify-center'>
         <div className='singleSearch flex items-center gap-2'>
           <label htmlFor='relevance' className='text-[#808080] font-semibold'>Sort by:</label>
-          <select name='' id=''></select>
+          <select name='' id='relevance' className='bg-white rounded-[3px] px-4 py-1'>
+            <option value=''>Relevance</option>
+          </select>
         </div>
       </div>
 
