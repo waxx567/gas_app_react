b90d57f20c7bbef650889dfa1b52ebff1235074e
waxx567 <waynem567@gmail.com>
2023-09-18 13:21:10 +0200

    Update Search.jsx

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -7,11 +7,12 @@ const Search = () => {
     <div className='searchDiv grid gp-10 bg-greyIsh rounded-[10px] p-[3rem]'>
       <form action=''>
         <div className='firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700'>
-            <div className='flex gap-2 items-center'>
-                <AiOutlineSearch className='text-[25px] icon'/>
-                <input type='text' className='bg-transparent text-blue-500 focus:outline-none w-[100%]' placeholder='Search products here' />
-                    <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6] hover:text-textColor icon'/>
-            </div>
+          <div className='flex gap-2 items-center'>
+              <AiOutlineSearch className='text-[25px] icon'/>
+              <input type='text' className='bg-transparent text-blue-500 focus:outline-none w-[100%]' placeholder='Search products here' />
+                <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6] hover:text-textColor icon'/>
+          </div>
+            
         </div>
       </form>
     </div>
