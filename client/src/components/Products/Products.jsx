faad041e831234bb1b05b337ec28dff20517c8eb
waxx567 <waynem567@gmail.com>
2023-09-18 16:21:33 +0200

    Add images

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -8,11 +8,13 @@ const Products = () => {
       <div className='prodContainer flex gap-10 justify-center flex-wrap items-center py-10'>
         <div className='group group/item singleProduct w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg'>
           <span className='flex justify-between items-center gap-4'>
-            <h1 className='text-[16px] font-semibold text-textColor group-hover:text-white'>5Kg gas bottle</h1>
-            <span className='flex items-center'>
-              <BiTimeFive/>Now
+            <h1 className='text-[16px] font-semibold text-textColor group-hover:text-white'>5Kg bottle</h1>
+            <span className='flex items-center text-[#ccc] gap-1'>
+              <BiTimeFive/>Available
             </span>
           </span>
+          <h6 className='text-[#ccc]'>R280.00</h6>
+          <img />
         </div>
       </div>
     </div>
