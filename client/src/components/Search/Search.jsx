eec7fa4088edd690f4ad7d03c7d7dd5a8369a129
waxx567 <waynem567@gmail.com>
2023-09-18 13:32:34 +0200

    Update Search.jsx

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
 
 import { AiOutlineCloseCircle, AiOutlineSearch } from 'react-icons/ai'
+import { BsHouseDoor } from 'react-icons/bs'
+import { CiLocationOn } from 'react-icons/ci'
 
 const Search = () => {
   return (
     <div className='searchDiv grid gp-10 bg-greyIsh rounded-[10px] p-[3rem]'>
       <form action=''>
         <div className='firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700'>
+
           <div className='flex gap-2 items-center'>
               <AiOutlineSearch className='text-[25px] icon'/>
-              <input type='text' className='bg-transparent text-blue-500 focus:outline-none w-[100%]' placeholder='Search products here' />
+              <input type='text' className='bg-transparent text-blue-500 focus:outline-none w-[100%]' placeholder='Search by product' />
                 <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6] hover:text-textColor icon'/>
           </div>
+
           <div className='flex gap-2 items-center'>
-              <AiOutlineSearch className='text-[25px] icon'/>
-              <input type='text' className='bg-transparent text-blue-500 focus:outline-none w-[100%]' placeholder='Search products here' />
+              <BsHouseDoor className='text-[25px] icon'/>
+              <input type='text' className='bg-transparent text-blue-500 focus:outline-none w-[100%]' placeholder='Search by company' />
                 <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6] hover:text-textColor icon'/>
           </div>
+
           <div className='flex gap-2 items-center'>
-              <AiOutlineSearch className='text-[25px] icon'/>
-              <input type='text' className='bg-transparent text-blue-500 focus:outline-none w-[100%]' placeholder='Search products here' />
+              <CiLocationOn className='text-[25px] icon'/>
+              <input type='text' className='bg-transparent text-blue-500 focus:outline-none w-[100%]' placeholder='Search by location' />
                 <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6] hover:text-textColor icon'/>
           </div>
+
         </div>
       </form>
     </div>
