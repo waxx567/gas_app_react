9b2e2f3fdecfe2bd1e38f132cc8659b58571a461
waxx567 <waynem567@gmail.com>
2023-09-18 15:43:06 +0200

    Update Products.jsx

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
+import { BiTimeFive } from 'react-icons/bi'
+
 const Products = () => {
   return (
     <div>
-      Products
+      <div className='prodContainer flex gap-10 justify-center flex-wrap items-center py-10'>
+        <div className='group group/item singleProduct w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg'>
+          <span className='flex justify-between items-center gap-4'>
+            <h1 className='text-[16px] font-semibold text-textColor group-hover:text-white'>5Kg gas bottle</h1>
+            <span>
+              <BiTimeFive/>
+            </span>
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
