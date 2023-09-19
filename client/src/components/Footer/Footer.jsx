0a0afd904c0b956843c720c3f178be0593e77377
waxx567 <waynem567@gmail.com>
2023-09-19 09:19:23 +0200

    Update Footer.jsx

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 // Import icons
-import { AiOutlineMail, AiOutlinePhone } from 'react-icons/ai'
-import { CiLocationOn } from 'react-icons/ci'
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
+import { BsFacebook } from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div className='footer p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-[8] grid grid-cols-5 m-auto items-center justify-center'>
+    <div className='footer p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-[8] grid grid-cols-3 m-auto items-center justify-center'>
 
       <div>
         <div className='logoDiv'>
@@ -34,43 +34,21 @@ const Footer = () => {
 
       <div className='grid'>
         <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
-          Resources
+          Contact Info
         </span>
 
-        <div className='grid gap-3'>
-          <li className='text-white opacity-[.7] hover:opacity-[1]'>Account</li>
-          <li className='text-white opacity-[.7] hover:opacity-[1]'>Support Center</li>
-          <li className='text-white opacity-[.7] hover:opacity-[1]'>Feedback</li>
-          <li className='text-white opacity-[.7] hover:opacity-[1]'>Contact</li>
+        <div>
+          <small className='text-[14px] text-white '>
+            email@mailbox.co.za
+          </small>
+          <div className='icons flex gap-4 py-[1rem]'>
+            <AiFillInstagram className='bg-white p-[8px] h-[35px] w-[35px] rounded-full icon text-blueColor'/>
+            <BsFacebook className='bg-white p-[8px] h-[35px] w-[35px] rounded-full icon text-blueColor'/>
+            <AiOutlineTwitter className='bg-white p-[8px] h-[35px] w-[35px] rounded-full icon text-blueColor'/>
+          </div>
         </div>
       </div>
 
-      <div className='grid'>
-        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
-          Support
-        </span>
-
-        <div className='grid gap-3'>
-          <li className='text-white opacity-[.7] hover:opacity-[1]'>Events</li>
-          <li className='text-white opacity-[.7] hover:opacity-[1]'>Promo</li>
-          <li className='text-white opacity-[.7] hover:opacity-[1]'>Req Demo</li>
-          <li className='text-white opacity-[.7] hover:opacity-[1]'>Careers</li>
-        </div>
-      </div>
-
-      <div className='grid'>
-        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
-          Support
-        </span>
-
-        <div className='grid gap-3'>
-          <li className='text-white opacity-[.7] hover:opacity-[1]'>Events</li>
-          <li className='text-white opacity-[.7] hover:opacity-[1]'>Promo</li>
-          <li className='text-white opacity-[.7] hover:opacity-[1]'>Req Demo</li>
-          <li className='text-white opacity-[.7] hover:opacity-[1]'>Careers</li>
-        </div>
-      </div>
-      
     </div>
   )
 }
