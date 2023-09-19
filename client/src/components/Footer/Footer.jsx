d0478b8f09aa7afe1190d2041b68a960eb40c26f
waxx567 <waynem567@gmail.com>
2023-09-19 08:48:48 +0200

    Update Footer.jsx

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -6,46 +6,56 @@ import { CiLocationOn } from 'react-icons/ci'
 
 const Footer = () => {
   return (
-    <div className='p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-[8] grid grid-cols-2 m-auto items-center justify-center'>
+    <div className='footer p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-[8] grid grid-cols-5 m-auto items-center justify-center'>
 
-      <div className='logoDiv w-[200px]'>
-        <h1 className='logo text-[25px] text-white pb-[1.5rem]'>
-        Go<strong>GAS</strong>Go
-        </h1>
-        <p className='text-[12px] text-white pb-[13px] opacity-70 leading-7'>
-        We are committed to providing the best experience to suit your needs
+      <div>
+        <div className='logoDiv'>
+          <h1 className='logo text-[25px] text-white pb-[1.5rem]'>
+            Go<strong>GAS</strong>Go
+          </h1>
+        </div>
+        <p className='text-white pb-[13px] opacity-70 leading-7'>
+          We are committed to providing the best experience to suit your needs. Our customers are our top priority.
         </p>
       </div>
 
-      <div className='grid w-[250px] gap-3'>
+      <div className='grid'>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
+          Company
+        </span>
 
-        <div className='flex'>
-          <p className='text-white opacity-70 leading-7 items-center pt-1.5'>
-            <AiOutlineMail/>
-          </p>
-          <p className='text-white opacity-70 leading-7 ml-2'>
-            email@goGASgo.com
-          </p>   
+        <div className='grid gap-3'>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>About Us</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>Features</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>News</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>FAQ</li>
         </div>
-        
-        <div className='flex'>
-          <p className='text-white opacity-70 leading-7 items-center pt-1.5'>
-            <AiOutlinePhone/>          
-          </p>
-          <p className='text-white opacity-70 leading-7 ml-2'>
-            +27 87 700 8087          
-          </p>            
-        </div>
+      </div>
 
-        <div className='flex'>
-          <p className='text-white opacity-70 leading-7 items-center pt-1.5'>
-            <CiLocationOn/>         
-          </p>
-          <p className='text-white opacity-70 leading-7 ml-2'>
-            123 Main Street, Big City, Inlandia          
-          </p>
-        </div>
+      <div className='grid'>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
+          Company
+        </span>
 
+        <div className='grid gap-3'>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>About Us</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>Features</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>News</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>FAQ</li>
+        </div>
+      </div>
+
+      <div className='grid'>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
+          Company
+        </span>
+
+        <div className='grid gap-3'>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>About Us</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>Features</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>News</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>FAQ</li>
+        </div>
       </div>
 
     </div>
