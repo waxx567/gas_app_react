9d10e5e8006c029966cf238d08fdfa1cc94ee13b
waxx567 <waynem567@gmail.com>
2023-09-19 10:41:33 +0200

    Update index.js

---
+++ server/index.js
@@ -0,0 +1,10 @@
+// Dependencies
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const cors = require('cors')
+
+// 
+app.listen(3002, () => {
+    console.log('Server is running on port 3002')
+})
