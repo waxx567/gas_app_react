30958a3db176caaaecb7b6c68123e58fb8f445fd
waxx567 <waynem567@gmail.com>
2023-09-19 07:46:30 +0200

    Update Footer.jsx

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -7,9 +7,9 @@ import { CiLocationOn } from 'react-icons/ci'
 const Footer = () => {
   return (
     <div className='p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-[8] grid grid-cols-2 m-auto items-center justify-center'>
-      <div className='flex items-center'>
+      {/* <div className='flex items-center'> */}
 
-        <div className='logoDiv'>
+        <div className='logoDiv w-[200px]'>
           <h1 className='logo text-[25px] text-white pb-[1.5rem]'>
             Go<strong>GAS</strong>Go
           </h1>
@@ -18,19 +18,38 @@ const Footer = () => {
           </p>
         </div>
 
-        <div className='grid'>
-          <p className='text-white opacity-70 leading-7'>
-            <AiOutlineMail/> email@gogasgo.com
-          </p>
-          <p className='text-white opacity-70 leading-7'>
-            <AiOutlinePhone/> (087)7008087          
-          </p>
-          <p className='text-white opacity-70 leading-7'>
-            <CiLocationOn/> 123 Main Street, Big City, Inlandia          
-          </p>
+        <div className='grid w-[250px] gap-3'>
+
+          <div className='flex'>
+            <p className='text-white opacity-70 leading-7 items-center pt-1.5'>
+              <AiOutlineMail/>
+            </p>
+            <p className='text-white opacity-70 leading-7 ml-2'>
+              email@goGASgo.com
+            </p>   
+          </div>
+          
+          <div className='flex'>
+            <p className='text-white opacity-70 leading-7 items-center pt-1.5'>
+              <AiOutlinePhone/>          
+            </p>
+            <p className='text-white opacity-70 leading-7 ml-2'>
+              +27 87 700 8087          
+            </p>            
+          </div>
+
+          <div className='flex'>
+            <p className='text-white opacity-70 leading-7 items-center pt-1.5'>
+              <CiLocationOn/>         
+            </p>
+            <p className='text-white opacity-70 leading-7 ml-2'>
+              123 Main Street, Big City, Inlandia          
+            </p>
+          </div>
+
         </div>
 
-      </div>
+      {/* </div> */}
     </div>
   )
 }
