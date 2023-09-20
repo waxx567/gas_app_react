9f5178f75df27a221df255b613e4497f913dc743
waxx567 <waynem567@gmail.com>
2023-09-20 07:17:31 +0200

    Update NavBar.jsx

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,4 +1,31 @@
 import React from 'react'
+import About from '../pages/About/About'
+import Contact from '../pages/Contact/Contact'
+import Login from '../pages/Login/Login'
+import Register from '../pages/Register/Register'
+
+// Import router dom
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+// RouterProvider
+const router = createBrowserRouter([
+  {
+    path: '/about',
+    element: <span><About/></span>
+  },
+  {
+    path: '/contact',
+    element: <span><Contact/></span>
+  },
+  {
+    path: '/contact',
+    element: <span><Login/></span>
+  },
+  {
+    path: '/contact',
+    element: <span><Register/></span>
+  }
+])
 
 const NavBar = () => {
   return (
@@ -9,10 +36,10 @@ const NavBar = () => {
         </h1>
       </div>
       <div className='menu flex gap-8'>
-        <li className='menuList text-[#6f6f6f] hover:text-blueColor'>About</li>
-        <li className='menuList text-[#6f6f6f] hover:text-blueColor'>Contact</li>
-        <li className='menuList text-[#6f6f6f] hover:text-blueColor'>Login</li>
-        <li className='menuList text-[#6f6f6f] hover:text-blueColor'>Register</li>
+        <li className='menuList text-[#6f6f6f] hover:text-blueColor'><span><About/></span></li>
+        <li className='menuList text-[#6f6f6f] hover:text-blueColor'><span><Contact/></span></li>
+        <li className='menuList text-[#6f6f6f] hover:text-blueColor'><span><Login/></span></li>
+        <li className='menuList text-[#6f6f6f] hover:text-blueColor'><span><Register/></span></li>
       </div>
     </div>
   )
